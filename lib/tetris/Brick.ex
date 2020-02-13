65a552db4266d797e0a3805261a87a580a84da13
Askar Absemetov <askar.absemetov@zeiss.com>
2020-02-13 19:02:28 +0100

    add brick manipulations

--- lib/tetris/Brick.ex
+++ lib/tetris/Brick.ex
@@ -4,7 +4,7 @@ defmodule Tetris.Brick do
   def new(), do: __struct__()
 
   def new_random() do
-    %{
+    %__MODULE__{
       name: random_name(),
       location: {40, 0},
       rotation: random_rotation(),
@@ -23,4 +23,35 @@ defmodule Tetris.Brick do
   defp random_reflection() do
     [true, false] |> Enum.random()
   end
+
+  def right(brick) do
+    %{brick | location: point_right(brick.location)}
+  end
+
+  def left(brick) do
+    %{brick | location: point_left(brick.location)}
+  end
+
+  def down(brick) do
+    %{brick | location: point_down(brick.location)}
+  end
+
+  def point_left({x, y}) do
+    {x - 1, y}
+  end
+
+  def point_right({x, y}) do
+    {x + 1, y}
+  end
+
+  def point_down({x, y}) do
+    {x, y + 1}
+  end
+
+  def spin_90(brick) do
+    %{brick | rotation: rotate(brick.rotation)}
+  end
+
+  def rotate(270), do: 0
+  def rotate(degrees), do: degrees + 90
 end
