da271e0fb621a99782cc46d1489bc6b5e1fdaad5
Askar Absemetov <askar.absemetov@zeiss.com>
2020-02-13 18:34:35 +0100

    added test

---
+++ .elixir_ls/build/test/lib/tetris/ebin/tetris.app
@@ -0,0 +1,6 @@
+{application,tetris,
+             [{applications,[kernel,stdlib,elixir,logger]},
+              {description,"tetris"},
+              {modules,['Elixir.Tetris','Elixir.Tetris.Brick']},
+              {registered,[]},
+              {vsn,"0.1.0"}]}.
---
+++ test/brick_test.exs
@@ -0,0 +1,18 @@
+defmodule BrickTest do
+  use ExUnit.Case
+  import Tetris.Brick
+
+  test "Creates a new brick" do
+    assert new_brick().name == :i
+  end
+
+  test "Creates a new random brick" do
+    actual = new_random()
+
+    assert actual.name in [:i, :z, :l, :t, :o]
+    assert actual.rotation in [0, 90, 180, 270]
+    assert actual.reflection in [true, false]
+  end
+
+  def new_brick, do: Tetris.Brick.new()
+end
